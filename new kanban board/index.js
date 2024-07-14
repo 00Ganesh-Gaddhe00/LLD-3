801048ed39ff694e25deb1827e639aa13a2c2a4a
Ganesh <00mr.lg00@gmail.com>
2024-07-14 17:50:40 +0530

    lock/edit functionality

--- new kanban board/index.js
+++ new kanban board/index.js
@@ -10,65 +10,88 @@ let ticketmain = document.querySelector('.main_cont')
 
 let isModalthere = false;
 let isDeleteactive = false;
-   
-addBtn.addEventListener('click', function(){
-     if(isModalthere)  {
-        modal.style.display = 'none'
-        isModalthere=false;
-     } else{
 
-        modal.style.display = 'flex'
-        isModalthere = true
-     }
+addBtn.addEventListener('click', function () {
+   if (isModalthere) {
+      modal.style.display = 'none'
+      isModalthere = false;
+   } else {
 
-})
-
-deleteBtn.addEventListener('click', function(){
-   if(!isDeleteactive){
-    deleteBtn.style.color = 'red'
-    isDeleteactive = true;
-   }else{
-      deleteBtn.style.color = 'rgba(0, 0, 0, 0.697)'
-      isDeleteactive=false
+      modal.style.display = 'flex'
+      isModalthere = true
    }
 
 })
 
-textarea.addEventListener('keydown', function(e){
-//    console.log(e.key)
-   if(e.key== "Enter"){
-    // console.log(textarea.value)
+deleteBtn.addEventListener('click', function () {
+   if (!isDeleteactive) {
+      deleteBtn.style.color = 'red'
+      isDeleteactive = true;
+   } else {
+      deleteBtn.style.color = 'rgba(0, 0, 0, 0.697)'
+      isDeleteactive = false
+   }
+
+})
+
+textarea.addEventListener('keydown', function (e) {
+   //    console.log(e.key)
+   if (e.key == "Enter") {
+      // console.log(textarea.value)
       createTicket(textarea.value)
 
-       modal.style.display = "none"
-       isModalthere = false
-        textarea.value = ""
-         
+      modal.style.display = "none"
+      isModalthere = false
+      textarea.value = ""
+
    }
 
 })
 
 let uid = new ShortUniqueId();
 
-function createTicket(taskText){
-  let id = uid.rnd()
+function createTicket(taskText) {
+   let id = uid.rnd()
 
-  let ticketcont = document.createElement('div')
-  ticketcont.className = "ticket_cont"
-  ticketcont.innerHTML = `<div class="ticket_color"></div>
+   let ticketcont = document.createElement('div')
+   ticketcont.className = "ticket_cont"
+   ticketcont.innerHTML = `<div class="ticket_color"></div>
                           <div class="ticket_id">#${id}</div>
-                   <div class="ticket_text">${taskText}</div>`
+                   <div class="ticket_text">${taskText}</div>
+                   <div class="lock">
+                    <i class="fa-solid fa-lock"></i>
+                   </div>`
 
-//    console.log(ticketcont)    
+   //    console.log(ticketcont)    
 
 
-   ticketmain.appendChild(ticketcont)  
-   
-   ticketcont.addEventListener('dblclick', function(){
-    if(isDeleteactive){
-        ticketcont.remove();
-    }
+   ticketmain.appendChild(ticketcont)
+
+   ticketcont.addEventListener('dblclick', function () {
+      if (isDeleteactive) {
+         ticketcont.remove();
+      }
    })
+
+   let lock = document.querySelector('.lock i')
+let ticketText = document.querySelector('.ticket_text')
+
+
+
+lock.addEventListener('click', function () {
+   if (lock.classList.contains('fa-lock')) {
+      lock.classList.remove('fa-lock')
+      lock.classList.add('fa-lock-open')
+      ticketText.setAttribute('contenteditable', 'true')
+   }
+   else {
+      lock.classList.remove('fa-lock-open')
+      lock.classList.add('fa-lock')
+      ticketText.setAttribute('contenteditable', 'false')
+   }
+
+})
+
 }
 
 
